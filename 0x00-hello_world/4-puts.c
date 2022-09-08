d0fbb57b77333edf168153a1008aaa5806229a14
Mickabtech <mickabtech@gmail.com>
2022-09-08 01:24:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: It will return O
+ *
+ *     */
+
+int main(void)
+{
+	  puts("\"programming is like building a multilingual puzzle");
+	      return (0);
+
+}
