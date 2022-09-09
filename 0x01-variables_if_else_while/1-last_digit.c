b10ffe01aabc8df5ff284aa4ad57aa5544c1c2e9
Mickabtech <mickabtech@gmail.com>
2022-09-09 09:54:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
+
 	int n;
 	int L;
 	char str[] = "Last digit of";
@@ -24,4 +25,5 @@ int main(void)
 		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 
 	return (0);
+
 }
