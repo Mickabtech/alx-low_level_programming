0b3533066b1351b3fea9665c11e25292d759a5f7
Mickabtech <mickabtech@gmail.com>
2022-09-20 04:12:45 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse.
+ * @s: pointer to string.
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+int length, last;
+
+length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+
+for (; last >= 0; last--)
+{
+_putchar(s[last]);
+}
+_putchar('\n');
+}
